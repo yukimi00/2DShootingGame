5976ce30bb1cd3bc29db08c3aae489cee85f19eb
yukimi <yukimi.dev@gmail.com>
2025-08-30 01:29:01 +0900

    プレイヤーが画面外に移動できないように制限を付けた

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -1,18 +1,33 @@
 using UnityEngine;
-using UnityEngine.UIElements;
 
 public class PlayerMove : MonoBehaviour {
     public float moveSpeed = 5f;
-    private float inputMoveX = 0f;
-    private float inputMoveY = 0f;
+    private float halfWidth, halfHeight;
+
+    void Start() {
+        // スプライトの半サイズを取得している
+        SpriteRenderer sr = GetComponent<SpriteRenderer>();
+        halfWidth = sr.bounds.extents.x;
+        halfHeight = sr.bounds.extents.y;
+    }
 
     void Update() {
         // 入力受付
-        inputMoveX = Input.GetAxis("Horizontal");
-        inputMoveY = Input.GetAxis("Vertical");
+        float inputMoveX = Input.GetAxis("Horizontal");
+        float inputMoveY = Input.GetAxis("Vertical");
 
         // 移動
         Vector3 movement = new Vector3(inputMoveX, inputMoveY, 0);
         transform.position += movement * moveSpeed * Time.deltaTime;
+
+        // 画面範囲の取得
+        Vector3 min = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, 0));
+        Vector3 max = Camera.main.ViewportToWorldPoint(new Vector3(1, 1, 0));
+
+        Vector3 pos = transform.position;
+        pos.x = Mathf.Clamp(pos.x, min.x + halfWidth, max.x - halfWidth);
+        pos.y = Mathf.Clamp(pos.y, min.y + halfHeight, max.y - halfHeight);
+
+        transform.position = pos;
     }
 }
