ca379c42eea011afb71076e7c69e0637099bb1aa
yukimi <yukimi.dev@gmail.com>
2025-09-01 01:46:06 +0900

    不要なimportを削除した

--- Assets/Scripts/Core/GameManager.cs
+++ Assets/Scripts/Core/GameManager.cs
@@ -1,6 +1,5 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.SocialPlatforms.Impl;
 
 public class GameManager : MonoBehaviour {
     public static GameManager Instance { get; private set; }
